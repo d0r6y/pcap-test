08d110fc93fff2d6909c5b95f764fb8af3bc2bba
d0r6y <solmontea98@gmail.com>
2020-07-27 13:24:19 -0700

    Ethernet src, dst address print

--- main.cpp
+++ main.cpp
@@ -2,16 +2,130 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <pcap.h>
+#include <arpa/inet.h>
 
 #define SIZE_ETHERNET 14
+
 
 const struct libnet_ethernet_hdr *ethernet;
 const struct libnet_ipv4_hdr *ip;
 const struct libnet_tcp_hdr *tcp;
 const u_char *payload;
 
-u_int size_ip;
-u_int size_tcp;
+struct ethernet_addr{
+	u_char byte[6];
+};
+
+struct ip_addr{
+	u_char byte[4];
+};
+uint8_t size_ip;
+uint8_t size_tcp;
+
+/*
+ * Function that prints MAC Address
+ * 
+ * Logic
+ * 1. Assigns input (packet Raw data) into predefined Ethernet Header Structure
+ * 2. Get ethernet_shost, ethernet_dhost field (MAC Address)
+ * 3. print
+ */
+void print_ethernet_header(const struct libnet_ethernet_hdr* eh){
+	
+	uint16_t ip_version = ntohs(eh->ether_type);
+
+	if(ip_version != 0x0800){
+		printf("IPv6 packet!!\n");
+		return;
+	}
+
+	printf("\n------------------------------------------------\n");
+	printf("               <Ethernet Header>               \n");
+	printf("       Dst MAC Addr [%02x:%02x:%02x:%02x:%02x:%02x]\n",
+		(eh->ether_dhost)[0],	
+		(eh->ether_dhost)[1],
+		(eh->ether_dhost)[2],
+		(eh->ether_dhost)[3],
+		(eh->ether_dhost)[4],
+		(eh->ether_dhost)[5]);
+
+	printf("       Src MAC Addr [%02x:%02x:%02x:%02x:%02x:%02x]\n",
+		(eh->ether_shost)[0],
+		(eh->ether_shost)[1],
+		(eh->ether_shost)[2],
+		(eh->ether_shost)[3],
+		(eh->ether_shost)[4],
+		(eh->ether_shost)[5]);
+	printf("------------------------------------------------\n");
+
+};
+
+/*
+ * Function that prints IP Address
+ * 
+ * Logic
+ * 1. Assigns input (packet Raw data) into predefined IP Header Structure
+ * 2. Get ip_src_addr, ip_dst_addr field (IP Address)
+ * 3. print
+ */
+ 
+ /*
+int print_ip_header(const struct libnet_ipv4_hdr *iph){
+	
+
+	printf("                  <IP Header>               \n");
+	printf("           SRC IP Addr [%d.%d.%d.%d]\n",
+		iph->ip_src.byte[0],
+		iph->ip_src.byte[1],
+		iph->ip_src.byte[2],
+		iph->ip_src.byte[3]);
+
+	printf("          DST IP Addr [%d.%d.%d.%d]\n",
+		iph->ip_dst.byte[0],
+		iph->ip_dst.byte[1],
+		iph->ip_dst.byte[2],
+		iph->ip_dst.byte[3]);
+	printf("------------------------------------------------\n");
+
+};
+*/
+/*
+ * Function that prints Port Number
+ * 
+ * Logic
+ * 1. Assigns input (packet Raw data) into predefined TCP Header Structure
+ * 2. Get tcp_src_port, tcp_dst_port field (Port Number)
+ * 3. Since byte-order can affect the output, use ntohs() function in order to print the right answer whether my cpu is Little Endian or Big Endian
+ * 4. print
+ */
+int print_tcp_header(const struct libnet_tcp_hdr *th){
+	
+	printf("                  <TCP Header>               \n");
+	printf("                Src Port : %d\n", ntohs(th->th_sport));
+	printf("                Dst Port : %d\n", ntohs(th->th_dport));
+	printf("------------------------------------------------\n");
+
+};
+
+/*
+ * Function that prints Data
+ * 
+ * Logic
+ * 1. print input (packet Raw data) in hexa decimal value ( ~ 32 byte long )
+ */
+void print_payload (const unsigned char * packet){
+	printf("                     <DATA>                   \n");
+	
+	int tmp = 32;
+	int count = 0;
+	while(tmp--){
+		printf("%02x ", *packet++);
+		if(++count%16 == 0){
+			printf("\n");
+		}
+	}
+	printf("================================================\n");
+};
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
 	printf("caplen : %d\n", header->caplen);
@@ -28,9 +142,11 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 	
 	payload = (const u_char*)(packet + SIZE_ETHERNET + size_ip + size_tcp);
 	
-	printf("\n%d %d", size_ip, size_tcp);
+	print_ethernet_header(ethernet);
+	//print_ip_header(ip);
+	//print_tcp_header(tcp);
+	//print_payload(payload);
 	
-	printf("\n%s",payload);
 }
 
 int main(int argc, char *argv[]){
@@ -78,11 +194,7 @@ int main(int argc, char *argv[]){
 	pcap_loop(handle, 10, got_packet, NULL);
 	
 	// ----------------------------
-	/* Grab a packet */
-	//packet = pcap_next(handle, &header);
-	/* Print its length */
-	//printf("Jacked a packet with length of [%d]\n", header.len);
-	/* And close the session */
+	
 	pcap_close(handle);
 	return(0);
 	
